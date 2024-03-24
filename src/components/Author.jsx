a5910cb61b0484b1d6969245cede9c557176131a
SagorKhan-Dev <sagorkhan.cit@gmail.com>
2024-03-24 13:35:26 +0600

    created component for content and author

---
+++ src/components/Author.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Author = () => {
+  return (
+    <div>
+      <h1>I am author</h1>
+    </div>
+  );
+};
+
+export default Author;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Blogs from "./pages/Blogs";
 import Bookmarks from "./pages/Bookmarks";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,12 @@ const router = createBrowserRouter([
         path: "/blogs",
         element: <Blogs />,
         loader: () => fetch("https://dev.to/api/articles?per_page=10&top=7"),
+      },
+      {
+        path: "/blog/:id",
+        element: <Blog />,
+        loader: ({ params }) =>
+          fetch(`https://dev.to/api/articles/${params.id}`),
       },
       {
         path: "/bookmarks",
